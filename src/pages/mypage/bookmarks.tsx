e7f023eefe95c1466d3f55e767e0af8cf3d23c5f
maeda <mk.eng1119@gmail.com>
2022-05-27 19:02:41 +0900

    routing変更

--- src/pages/mypage/bookmarks.tsx
+++ src/pages/mypage/bookmarks.tsx
@@ -1,33 +1,22 @@
 import { NextPage } from 'next'
+import { useWordSearch } from 'src/components/hooks/useWordSearch'
 import { Link } from 'src/components/ui-elements/Link'
 import { ArticleCard } from 'src/components/ui-parts/ArticleCard'
+import { WordSearchForm } from 'src/components/ui-parts/form/WordSearchForm'
 import { DefaultLayout } from 'src/components/ui-parts/layout/DefaultLayout'
+import { NoDataMessage } from 'src/components/ui-parts/NoDataMessage'
 
 const MyPage: NextPage = () => {
   // 自分が登録したブックマークを全件取得する
   // TODO: Pagination作成
   // TODO: 検索フォームを作成
 
-  //@ts-ignore
-  const dummyData = [...Array(6).keys()].map((val) => {
-    return {
-      id: `id ${val}`,
-      url: 'https://zenn.dev/furai_mountain/scraps/225734efc9ab7d',
-      title: '【React TypeScript Next.js】ログイン認証実装の際のメモ',
-      categories: [
-        { value: '1', label: 'JavaScript' },
-        { value: '2', label: 'TypeScript' },
-        { value: '3', label: 'React' },
-      ],
-      isRead: true,
-      comprehension: 3,
-      memo: 'aaaaaa ',
-    }
-  })
+  const { searchedBookmarks } = useWordSearch()
+
   return (
     <DefaultLayout title="マイページ">
       <div className="container">
-        <div className="mt-16 flex justify-center gap-16 ">
+        <div className="mt-12 flex justify-center gap-16 ">
           <Link
             href="/mypage/bookmarks"
             classes={['text-2xl text-gray-600 cursor-pointer']}
@@ -47,13 +36,16 @@ const MyPage: NextPage = () => {
             Setting
           </Link>
         </div>
-
-        <div className="p-8">
-          <div className="mt-20 flex flex-wrap justify-center gap-12">
-            {dummyData.map((bookmark, i) => (
+        <WordSearchForm />
+        {/* <LoginForm /> */}
+        <div className="mt-16 flex flex-wrap justify-center gap-12">
+          {searchedBookmarks.length > 0 ? (
+            searchedBookmarks?.map((bookmark, i) => (
               <ArticleCard bookmark={bookmark} key={bookmark.id + i} />
-            ))}
-          </div>
+            ))
+          ) : (
+            <NoDataMessage />
+          )}
         </div>
       </div>
     </DefaultLayout>
