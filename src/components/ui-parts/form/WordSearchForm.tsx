a85e22eab61ce117495093e9ee58ae9b1d4bb9de
maeda <mk.eng1119@gmail.com>
2022-05-27 19:03:36 +0900

    検索フォームコンポーネントを作成

---
+++ src/components/ui-parts/form/WordSearchForm.tsx
@@ -0,0 +1,26 @@
+import { ChangeEvent, FC } from 'react'
+import { useWordSearch } from 'src/components/hooks/useWordSearch'
+import { SearchIcon } from 'src/components/ui-elements/Icons/SearchIcon'
+
+export const WordSearchForm: FC = () => {
+  const { submitWordSearch, setSearchWord, searchWord, wordSearch } =
+    useWordSearch()
+
+  return (
+    <form
+      onSubmit={submitWordSearch}
+      className="relative mx-auto mt-12 w-10/12"
+    >
+      <input
+        className="h-16 w-full rounded-full border-2 border-gray-200  bg-white bg-opacity-70 px-6 shadow-lg focus:border-asset-green focus:bg-white focus:outline-none"
+        type="text"
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setSearchWord(e.target.value)
+        }
+        value={searchWord}
+        placeholder="キーワードを入力..."
+      />
+      <SearchIcon onClick={wordSearch} />
+    </form>
+  )
+}
