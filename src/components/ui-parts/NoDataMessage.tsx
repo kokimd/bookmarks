a0cbb11a529b4ad460c67a421a834462860e7ec0
maeda <mk.eng1119@gmail.com>
2022-05-27 19:05:50 +0900

    検索結果ないし、データがない場合の表示コンポーネント作成

--- src/components/ui-parts/NoDataMessage.tsx
+++ src/components/ui-parts/NoDataMessage.tsx
@@ -2,14 +2,11 @@ import Image from 'next/image'
 import { FC } from 'react'
 import { Typography } from '../ui-elements/Typography/Typography'
 
-type Props = {
-  searchedWord: string
-}
-export const NoDataMessage: FC<Props> = ({ searchedWord }) => {
+export const NoDataMessage: FC = () => {
   return (
     <div className=" flex w-10/12 flex-col items-center">
       <Typography variant={'h4'} classes={['opacity-75']}>
-        「{searchedWord}」の検索結果が見つかりませんでした。
+        検索結果が見つかりませんでした。
       </Typography>
       <div className="mt-16">
         <Image
