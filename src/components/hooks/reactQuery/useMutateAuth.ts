7654313e735f82d61cfd615d86654609c56aed25
maeda <mk.eng1119@gmail.com>
2022-05-28 15:40:07 +0900

    ログイン・会員登録のhooksを作成

---
+++ src/components/hooks/reactQuery/useMutateAuth.ts
@@ -0,0 +1,67 @@
+import { useMutation } from 'react-query'
+import { supabase } from 'src/utils/supabase'
+import type { ResponseError } from 'superagent'
+import { AuthData } from 'src/utils/types'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { LoginSchema } from 'src/utils/validationSchema'
+
+export const useAuthMutation = () => {
+  const [isLogin, setIsLogin] = useState(true)
+
+  // 新規登録
+  const registerMutate = useMutation(
+    async (payload: AuthData) => {
+      const { email, password } = payload
+      const { error } = await supabase.auth.signUp({ email, password })
+      if (error) throw new Error(`${error.status}「${error.message}」`)
+    },
+    {
+      onError: (err: ResponseError) => {
+        alert(err.message)
+      },
+    },
+  )
+
+  // ログイン
+  const loginMutate = useMutation(
+    async (payload: AuthData) => {
+      const { email, password } = payload
+      const { error } = await supabase.auth.signIn({ email, password })
+      if (error) throw new Error(`${error.status}「${error.message}」`)
+    },
+    {
+      onError: (err: ResponseError) => {
+        alert(err.message)
+      },
+    },
+  )
+
+  const defaultValues = {
+    email: '',
+    password: '',
+  }
+
+  const methods = useForm<AuthData>({
+    defaultValues,
+    resolver: yupResolver(LoginSchema),
+  })
+
+  const authSubmit = (payload: AuthData) => {
+    if (isLogin) {
+      loginMutate.mutate(payload)
+    } else {
+      registerMutate.mutate(payload)
+    }
+  }
+
+  return {
+    isLogin,
+    setIsLogin,
+    methods,
+    authSubmit,
+    registerMutate,
+    loginMutate,
+  }
+}
