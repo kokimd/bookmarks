b85b3a96ee17be9b36b09ef546fac21cd84bc87b
maeda <mk.eng1119@gmail.com>
2022-05-28 15:44:37 +0900

    react-queryの設定とsession管理処理の追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { useSession } from 'src/components/hooks/useSession'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useSession()
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnWindowFocus: false,
+        suspense: true,
+      },
+    },
+  })
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
