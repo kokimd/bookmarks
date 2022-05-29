67274666ec411bf0cad2231252480af7400a59ff
maeda <mk.eng1119@gmail.com>
2022-05-29 16:25:36 +0900

    認証によるハンドリングを追加

--- src/components/ui-parts/layout/Header.tsx
+++ src/components/ui-parts/layout/Header.tsx
@@ -1,7 +1,13 @@
 import { FC } from 'react'
+import { useMutateAuth } from 'src/hooks/reactQuery/useMutateAuth'
+import { Button } from 'src/components/ui-elements/button/Button'
 import { Link } from 'src/components/ui-elements/Link'
+import useStore from 'src/store'
 
 export const Header: FC = () => {
+  const { logout } = useMutateAuth()
+  const session = useStore((state) => state.session)
+
   return (
     <header className="w-full bg-white">
       <nav className="container mx-auto flex w-full flex-wrap items-center justify-between p-6 text-asset-green">
@@ -11,24 +17,33 @@ export const Header: FC = () => {
           </Link>
         </div>
         <div className="ml-auto flex gap-8">
-          <Link
-            href="/login"
-            classes={['text-white bg-asset-green rounded-md']}
-          >
-            Login
-          </Link>
-          <Link
-            href="/mypage/create"
-            classes={['text-white bg-asset-green rounded-md']}
-          >
-            Add New
-          </Link>
-          <Link
-            href="/mypage/bookmarks"
-            classes={['text-white bg-asset-green rounded-md']}
-          >
-            MyPage
-          </Link>
+          {!session && (
+            <Link
+              href="/login"
+              classes={['text-white bg-asset-green rounded-md']}
+            >
+              Log in
+            </Link>
+          )}
+          {session && (
+            <>
+              <Link
+                href="/mypage/create"
+                classes={['text-white bg-asset-green rounded-md']}
+              >
+                Add New
+              </Link>
+              <Link
+                href="/mypage/bookmarks"
+                classes={['text-white bg-asset-green rounded-md']}
+              >
+                MyPage
+              </Link>
+              <Button color={'danger'} onClick={logout}>
+                Logout
+              </Button>
+            </>
+          )}
         </div>
       </nav>
     </header>
