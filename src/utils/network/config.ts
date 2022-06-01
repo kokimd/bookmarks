ebf163d58f252ffbd375649932a94d3443fff208
maeda <mk.eng1119@gmail.com>
2022-06-01 22:27:29 +0900

    axios修正

--- src/utils/network/config.ts
+++ src/utils/network/config.ts
@@ -5,13 +5,6 @@ export const DOMAIN = 'http://localhost:3000'
 const createAxiosClient = () =>
   axiosBase.create({
     baseURL: DOMAIN,
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Requested-With': 'XMLHttpRequest',
-    },
-    responseType: 'json',
-    timeout: 15 * 1000,
-    withCredentials: true,
   })
 
 export const API = createAxiosClient()
