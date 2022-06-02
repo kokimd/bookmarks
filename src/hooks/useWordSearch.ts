3a8bee5ec4c125f7e00642b4a98867d629994d47
maeda <mk.eng1119@gmail.com>
2022-06-02 21:12:45 +0900

    undefined考慮

--- src/hooks/useWordSearch.ts
+++ src/hooks/useWordSearch.ts
@@ -3,40 +3,43 @@ import useStore from 'src/store'
 import { useQueryBookmarks } from './reactQuery/useQueryBookmarks'
 
 export const useWordSearch = () => {
-  const { data: bookmarks } = useQueryBookmarks()
   const searchedBookmarks = useStore((state) => state.searchedBookmarks)
   const setSearchedBookmarks = useStore((state) => state.setSearchedBookmarks)
   const searchWord = useStore((state) => state.searchWord)
   const setSearchWord = useStore((state) => state.setSearchWord)
   const deleteSearchWord = useStore((state) => state.deleteSearchWord)
+  const { data: bookmarks } = useQueryBookmarks()
 
   useEffect(() => {
-    setSearchedBookmarks(bookmarks)
+    bookmarks && setSearchedBookmarks(bookmarks)
 
     return () => deleteSearchWord()
   }, [])
 
   // 検索処理
   const wordSearch = () => {
-    // タイトルから検索
-    const titleFilteredList = bookmarks.filter((data) =>
-      data.title.toUpperCase().includes(searchWord.toUpperCase()),
-    )
+    if (bookmarks) {
+      // タイトルから検索
+      const titleFilteredList = bookmarks.filter((data) =>
+        data.title.toUpperCase().includes(searchWord.toUpperCase()),
+      )
 
-    // カテゴリから検索
-    const categoryFilteredList = bookmarks.filter((data) =>
-      data.categories.some(
-        (category) => category.label.toUpperCase() === searchWord.toUpperCase(),
-      ),
-    )
-    // マージし、重複を取り除く
-    const mergeList = [...titleFilteredList, ...categoryFilteredList]
-    const resultList = Array.from(
-      mergeList
-        .reduce((map, current) => map.set(current.id, current), new Map())
-        .values(),
-    )
-    setSearchedBookmarks(resultList)
+      // カテゴリから検索
+      const categoryFilteredList = bookmarks.filter((data) =>
+        data.categories.some(
+          (category) =>
+            category.name.toUpperCase() === searchWord.toUpperCase(),
+        ),
+      )
+      // マージし、重複を取り除く
+      const mergeList = [...titleFilteredList, ...categoryFilteredList]
+      const resultList = Array.from(
+        mergeList
+          .reduce((map, current) => map.set(current.id, current), new Map())
+          .values(),
+      )
+      setSearchedBookmarks(resultList)
+    }
   }
 
   // onSubmitでの検索処理
